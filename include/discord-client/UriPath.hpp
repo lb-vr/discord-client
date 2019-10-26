7a399875773412100f2f3eaa25ab41c73356cb45
Shirao Shotaro <shirao.shotaro@gmail.com>
2019-10-27 00:29:49 +0900

    add UriPath

---
+++ include/discord-client/UriPath.hpp
@@ -0,0 +1,31 @@
+#ifndef LBVR_URIPATH_HPP
+#define LBVR_URIPATH_HPP
+
+#include <string>
+#include <unordered_map>
+
+namespace lbvr {
+
+class UriPath {
+public:
+
+	UriPath(const std::string & path, const std::unordered_map<std::string, std::string> & args);
+	const std::string & getUri() const;
+	operator const std::string & () const;
+	
+	// const std::string bucket(const RequestMethod & method) const;
+
+
+private:
+	const std::string path_;
+	const std::unordered_map<std::string, std::string> args_;
+	std::string uri_;
+
+	//major parameters
+	Snowflake<Channel> channelID;
+	Snowflake<Server> serverID;
+};
+
+}
+
+#endif
---
+++ src/discord-client/UriPath.cpp
@@ -0,0 +1,32 @@
+#include "discord-client/UriPath.hpp"
+#include <cassert>
+
+lbvr::UriPath::UriPath(const std::string & path, const std::unordered_map<std::string, std::string>& args)
+	: path_(path), args_(args)
+{
+	// replace placeholder
+	this->uri_ = this->path_;
+	size_t start = std::string::npos;
+	size_t end = std::string::npos;
+	const auto _findNextPlaceholder = [&]() -> bool {
+		start = this->uri_.find('{');
+		end = this->uri_.find('}');
+		return (start != std::string::npos && end != std::string::npos);
+	};
+
+	while (_findNextPlaceholder()) {
+		const std::string placeholder = path.substr(start + 1, end - start - 1);
+		assert(args.count(placeholder));
+		const std::string value = args.at(placeholder);
+		this->uri_ = this->uri_.substr(0, start) + value + this->uri_.substr(end + 1);
+	}
+}
+
+const std::string & lbvr::UriPath::getUri() const {
+	return this->uri_;
+}
+
+lbvr::UriPath::operator const std::string & () const {
+	return this->getUri();
+}
+
