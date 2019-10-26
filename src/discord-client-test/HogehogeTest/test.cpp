08cc97794aa86078c038fc4217afbb4c7c29dbcc
Shirao Shotaro <shirao.shotaro@gmail.com>
2019-10-27 00:44:29 +0900

    disable sample test case

--- src/discord-client-test/HogehogeTest/test.cpp
+++ src/discord-client-test/HogehogeTest/test.cpp
@@ -4,11 +4,11 @@ int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
-
+/*
 TEST(HogehogeTest, TestCaseA) {
 	EXPECT_EQ(1, 2);
 }
 
 TEST(HogehogeTest, TestCaseB) {
 	EXPECT_EQ(2, 2);
-}
+}*/
