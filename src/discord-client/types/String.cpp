5afeea2888b825f5f6461b26bbc6e7c880a44967
Shirao Shotaro <shirao.shotaro@gmail.com>
2019-11-04 21:41:16 +0900

    String追加

---
+++ src/discord-client/types/String.cpp
@@ -0,0 +1,17 @@
+#include "..\..\..\include\discord-client\types\String.hpp"
+
+json11::Json lbvr::types::String::toJson(void) const {
+	return json11::Json(this->get());
+}
+
+std::string lbvr::types::String::toString(void) const {
+	return this->get();
+}
+
+json11::Json lbvr::types::NullableString::toJson(void) const {
+	return (this->isSet() ? json11::Json(this->get()) : json11::Json(nullptr));
+}
+
+std::string lbvr::types::NullableString::toString(void) const {
+	return (this->isSet() ? this->get() : "null");
+}
